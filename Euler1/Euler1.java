51d9cddabed97be2945d25cd9f7fbc0ddb916ae8
ChrisCM <chriscm.neu@gmail.com>
2017-04-09 14:39:50 +0100

    Euler1.java

--- Euler1/Euler1.java
+++ Euler1/Euler1.java
@@ -1,32 +1,18 @@
 
-public class Euler1 {
+public class ET1 {
 	public static void main (String [] args){
+		int base = 0;
 		int max = 1000;
-		int base3 = 1;
-		int base5 = 1;
-		int numerator3 = 3;
-		int numerator5 = 5;
-		int denominator3;
-		int denominator5;
 		int total = 0;
-		int total3 = 0;
-		int total5 = 0;
-				
-		do{
-			denominator3 = (base3%numerator3);
-			denominator5 = (base5%numerator5);
-			if(denominator3 == 0 && denominator5 == 0){
-				total3 = (total3 + base3);
-				total5 = (total5 + base5);
-				total = (total + base3 + base5);
-			}
-			base3++;
-			base5++;
-		}
-		while(base5 != max);
 		
-		System.out.println("Total for 3 = " + total3);
-		System.out.println("Total for 5 = " + total5);
-		System.out.println("Total for both = " + total);
+		do{
+			if(base%3 == 0){
+		
+			total = total+base;
+			}
+			base++;
+		}while (max != base);
+			
+		System.out.println("Total for 3 and 5: " + total);
 	}
 }
