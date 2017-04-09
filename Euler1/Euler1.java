c699655911648aeac7dc722b4f8f028a34b1856d
ChrisCM <chriscm.neu@gmail.com>
2017-04-09 14:56:48 +0100

    Added Final Answer

--- Euler1/Euler1.java
+++ Euler1/Euler1.java
@@ -1,18 +1,13 @@
 
-public class ET1 {
+public class Euler1 {
 	public static void main (String [] args){
-		int base = 0;
-		int max = 1000;
 		int total = 0;
 		
-		do{
-			if(base%3 == 0){
-		
-			total = total+base;
+		for (int i=3; i<1000; i++){
+			if(i%3 ==0 || i%5 == 0){
+				total = total + i;
 			}
-			base++;
-		}while (max != base);
-			
-		System.out.println("Total for 3 and 5: " + total);
+		}
+		System.out.print(total);
 	}
 }
